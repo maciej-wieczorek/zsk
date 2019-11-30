8f813b930fef53c8c3370b3705f93ee1ea446bb1
maciej-wieczorek <38870449+maciej-wieczorek@users.noreply.github.com>
2019-11-30 23:13:36 +0100

    sets

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ pf.forEach(element => {
 function flip(element) {
     element.style.transform += "rotateX(180deg)";
 }
+
+var w = document.querySelectorAll(".words");
+var wr = document.querySelector("#words_wrapper");
+
+function add_words_on_enter() {
+    w.forEach(element => {
+        element.addEventListener("keypress", function(e) {
+            var key = e.which || e.keyCode;
+            if (key === 13) {
+                add_words();
+            }
+        })
+    });
+}
+
+add_words_on_enter();
+
+function add_words() {
+    wr.innerHTML += "<br><input type='tex' class='words' name='word' placeholder='Słowo'><input type='text' class='words' name='translation' placeholder='Tłumaczenie'>";
+}
